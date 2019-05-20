06f818ad80387f585bec4c051800574916add580
Pedram Foroughi <foroughi.pedram@gmail.com>
2019-05-20 10:29:55 +0300

    Add LaughPresenter
    Added Laugh presenter class which implements the contract presenter interface

---
+++ app/src/main/java/com/techspark/laughterapp/LaughPresenter.kt
@@ -0,0 +1,13 @@
+package com.techspark.laughterapp
+
+class LaughPresenter(view: Contract.View) : Contract.Presenter{
+
+
+    override fun load() {
+
+    }
+
+    override fun play() {
+    }
+
+}
