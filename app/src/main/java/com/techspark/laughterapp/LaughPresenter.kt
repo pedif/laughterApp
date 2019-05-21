235a2bf9b1e46f88870f55b97af599f96a2ed6ff
Pedram Foroughi <foroughi.pedram@gmail.com>
2019-05-21 10:10:30 +0300

    Update LaughPresenter.kt
    Updated play function to pass the test

--- app/src/main/java/com/techspark/laughterapp/LaughPresenter.kt
+++ app/src/main/java/com/techspark/laughterapp/LaughPresenter.kt
@@ -2,12 +2,10 @@ package com.techspark.laughterapp
 
 class LaughPresenter(private val view: Contract.View) : Contract.Presenter{
 
-    var currentIndex: Int = 0
 
-    private val sounds = listOf<Int>(R.raw.test1)
-
-
+    val sounds = listOf<Int>(R.raw.test1)
     override fun play(index: Int) {
 
+        view.play(sounds[index])
     }
 }
