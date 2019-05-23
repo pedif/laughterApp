f9a651e999435b35be83c62ca3468dfd4bde3231
Pedram Foroughi <foroughi.pedram@gmail.com>
2019-05-23 11:57:07 +0300

    Update LaughFragment.kt
    Added a variable to hold the id of the media which is being played

--- app/src/main/java/com/techspark/laughterapp/LaughFragment.kt
+++ app/src/main/java/com/techspark/laughterapp/LaughFragment.kt
@@ -1,6 +1,7 @@
 package com.techspark.laughterapp
 
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,9 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 
 
-
-
 class LaughFragment : Fragment(),Contract.View {
+
+    lateinit var  player: MediaPlayer
+    var sound: Int? = null
+
+    override fun play(id: Int) {
+        sound= id
+//        player = MediaPlayer.create(activity,id)
+//        player.start()
+    }
 
     lateinit var presenter: Contract.Presenter
 
@@ -20,7 +28,10 @@ class LaughFragment : Fragment(),Contract.View {
     ): View? {
 
         presenter = LaughPresenter(this)
-        return inflater.inflate(R.layout.fragment_laugh, container, false)
+
+        val rootView: View = inflater.inflate(R.layout.fragment_laugh, container, false)
+
+        return rootView
     }
 
 
