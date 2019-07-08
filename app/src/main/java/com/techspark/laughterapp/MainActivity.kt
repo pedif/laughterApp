2b38135af610d8df98b0f4a2c70d8316a2736022
Pedram <foroughi.pedram@gmail.com>
2019-07-08 10:29:04 +0300

    Add admob Banner
    Added and loaded a test admob banner to the main activity

--- app/src/main/java/com/techspark/laughterapp/MainActivity.kt
+++ app/src/main/java/com/techspark/laughterapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.techspark.laughterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this,resources.getString(R.string.ad_id) )
+
+        val adReq =  AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR ).build()
+
+        adView.loadAd(adReq)
+
     }
 }
