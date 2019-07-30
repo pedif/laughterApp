26f48f48aa1af153ad79c34c0bddfe21aff84d85
Pedram <foroughi.pedram@gmail.com>
2019-07-30 09:50:04 +0300

    Update Laugh Presenter
    Updated play function to now log an event for Firebase

--- app/src/main/java/com/techspark/laughterapp/LaughPresenter.kt
+++ app/src/main/java/com/techspark/laughterapp/LaughPresenter.kt
@@ -24,5 +24,11 @@ class LaughPresenter(private val view: Contract.View, private val context: Conte
     override fun play(index: Int) {
 
         view.play(sounds[index])
+
+        //Logs an event for every click of buttons with the index of that button
+        val bundle = Bundle()
+        bundle.putInt(FirebaseAnalytics.Param.ITEM_ID,index)
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT,bundle)
+
     }
 }
