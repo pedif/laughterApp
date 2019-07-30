745df3f15c4fd29f5bacfa97776474452f9b3c2a
Pedram <foroughi.pedram@gmail.com>
2019-07-30 09:50:34 +0300

    Update Main Activity
    Updated ad view Id

--- app/src/main/java/com/techspark/laughterapp/MainActivity.kt
+++ app/src/main/java/com/techspark/laughterapp/MainActivity.kt
@@ -2,8 +2,6 @@ package com.techspark.laughterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         val adReq =  AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR ).build()
 
-        adView.loadAd(adReq)
+        ad_view.loadAd(adReq)
 
     }
 }
